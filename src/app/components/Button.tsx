6dfa181c3896cd2ee6aa5540123758485cd19337
Nathaniel Quansah <n.bongo40@gmail.com>
2024-06-18 22:33:08 +0000

    fixed conflicts

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -9,10 +9,9 @@ interface Props {
     loading?: boolean;
     styles?: any
     icon?: string
-    contentClassName?:string
 }
 
-const Button: FC<Props> = ({ text, className, handleClick, loading, styles, icon, contentClassName }) => {
+const Button: FC<Props> = ({ text, className, handleClick, loading, styles, icon }) => {
     const [isLoading, setIsLoading] = useState(false); // State for loading indicator
 
     const onClickHandler = () => {
@@ -25,12 +24,12 @@ const Button: FC<Props> = ({ text, className, handleClick, loading, styles, icon
     return (
         <button
             onClick={onClickHandler}
-            className={`bg-gradient-to-b from-[#3384D9] via-[#0051A6] to-[#073E66] text-white text-sm px-2 py-3 rounded-lg ${className}`}
+            className={`bg-gradient-to-b from-[#3384D9] via-[#0051A6] to-[#073E66] text-white text-sm px-4 py-3 rounded-lg ${className}`}
             disabled={isLoading} // Disable button when loading
             style={styles}
         >
-            {isLoading ? 'Loading...' : <div className={`flex flex-row justify-center items-center space-x-3 ${contentClassName}`}>
-                <span className='mx-3'>{text}</span>
+            {isLoading ? 'Loading...' : <div className='flex flex-row justify-center items-center space-x-3'>
+                <span>{text}</span>
                 {icon && <Image src={icon} alt="earn" width={20} />}
             </div>
             }
