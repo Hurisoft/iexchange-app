3d8e9a6bd67cd19081b6e968775cc01435bf5287
Jovells <jovells.appiah@gmail.com>
2024-06-19 21:29:38 +0000

    added contract address

--- src/app/zkPass/constants.ts
+++ src/app/zkPass/constants.ts
@@ -1,3 +1,3 @@
 export const EVMTaskAllocator = "0x19a567b3b212a5b35bA0E3B600FbEd5c2eE9083d";
 export const ZKPASS_APP_ID = "632250ba-3fa7-4c72-92d2-f21675a7c338";
-export const ZKPASS_SCHEMA_ID = "b5181543e74c4c51879d8b3112368d45";
+export const ZKPASS_SCHEMA_ID = "bb0841c23af949fb81641c49ffa1e189";
--- src/app/zkPass/page.tsx
+++ src/app/zkPass/page.tsx
@@ -8,50 +8,81 @@ import TransgateConnect from "@zkpass/transgate-js-sdk";
 import { Dialog } from "@/components/ui/dialog";
 import { DialogContent } from "@radix-ui/react-dialog";
 import { Button } from "../components";
+import { ethers } from "ethers";
+import AttestationABI from "./AttestationABI.json";
 
 export default function Home() {
   const [result, setResult] = useState<any>();
   const [showPopup, setShowPopup] = useState<boolean>(false);
   const [newSchemaId, setNewSchemaId] = useState<string>("");
   const [newAppId, setNewAppId] = useState<string>("");
-
-  const start = async (schemas = [ZKPASS_SCHEMA_ID], appid = ZKPASS_APP_ID) => {
+  const start = async (
+    schemaId: string = ZKPASS_SCHEMA_ID,
+    appid: string = ZKPASS_APP_ID
+  ) => {
     if (newSchemaId) {
-      schemas = [newSchemaId];
+      schemaId = newSchemaId;
     }
     if (newAppId) {
       appid = newAppId;
     }
-    console.log("appId ", appid, " schemasID", schemas[0]);
     try {
       const connector = new TransgateConnect(appid);
-
       const isAvailable = await connector.isTransgateAvailable();
       if (!isAvailable) {
-        setShowPopup(true);
-        return;
+        return alert("Please install zkPass TransGate");
       }
-
-      const resultList: any[] = [];
-      while (schemas.length > 0) {
-        const schemaId = schemas.shift() as string;
-
-        const res = (await connector.launch(schemaId)) as Result;
-        resultList.push(res);
-
-        const verifyResult = verifyEVMMessageSignature(
-          res.taskId,
-          schemaId,
-          res.uHash,
-          res.publicFieldsHash,
-          res.validatorSignature,
-          res.validatorAddress
-        );
-        console.log("verifyResult", verifyResult);
+      //@ts-ignore
+      if (window.ethereum == null) {
+        return alert("MetaMask not installed");
       }
-      if (resultList.length === 1) {
-        setResult(resultList);
-      }
+      //@ts-ignore
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+      //get your ethereum address
+      const account = await signer.getAddress();
+
+      const res: any = await connector.launch(schemaId, account);
+      setResult(res);
+
+      //Sepolia contract address
+      //You can add from https://chainlist.org/?search=11155111&testnets=true
+      const contractAddress = "0x8c18c0436A8d6ea44C87Bf5853F8D11B55CF0302";
+
+      const taskId = ethers.hexlify(ethers.toUtf8Bytes(res.taskId)); // to hex
+      schemaId = ethers.hexlify(ethers.toUtf8Bytes(schemaId)); // to hex
+
+      const chainParams = {
+        taskId,
+        schemaId,
+        uHash: res.uHash,
+        recipient: account,
+        publicFieldsHash: res.publicFieldsHash,
+        validator: res.validatorAddress,
+        allocatorSignature: res.allocatorSignature,
+        validatorSignature: res.validatorSignature,
+      };
+      console.log("chainParams", chainParams);
+
+      const contract = new ethers.Contract(
+        contractAddress,
+        AttestationABI,
+        provider
+      );
+      const data = contract.interface.encodeFunctionData("attest", [
+        chainParams,
+      ]);
+
+      let transaction = {
+        to: contractAddress,
+        from: account,
+        value: 0,
+        data,
+      };
+      console.log("transaction", transaction);
+      let tx = await signer?.sendTransaction(transaction);
+      console.log("transaction hash====>", tx.hash);
+      alert("Transaction sent successfully!");
     } catch (err) {
       alert(JSON.stringify(err));
       console.log("error", err);
@@ -66,20 +97,26 @@ export default function Home() {
         </h2>
         <div className="grid grid-cols-1 gap-6">
           <div className="flex justify-center">
-            <input
-              type="text"
-              placeholder={ZKPASS_SCHEMA_ID}
-              className="px-4 py-2 bg-gray-700 text-white rounded-md text-sm font-medium"
-              value={newSchemaId}
-              onChange={(e) => setNewSchemaId(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder={ZKPASS_APP_ID}
-              className="px-4 py-2 bg-gray-700 text-white rounded-md text-sm font-medium"
-              value={newAppId}
-              onChange={(e) => setNewAppId(e.target.value)}
-            />
+            <div className="flex flex-col space-y-4">
+              <p className="text-white text-sm font-medium">Schema ID</p>
+              <input
+                type="text"
+                placeholder={ZKPASS_SCHEMA_ID}
+                className="px-4 py-2 bg-gray-700 text-white rounded-md text-sm font-medium"
+                value={newSchemaId}
+                onChange={(e) => setNewSchemaId(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col space-y-4">
+              <p className="text-white text-sm font-medium">App ID</p>
+              <input
+                type="text"
+                placeholder={ZKPASS_APP_ID}
+                className="px-4 py-2 bg-gray-700 text-white rounded-md text-sm font-medium"
+                value={newAppId}
+                onChange={(e) => setNewAppId(e.target.value)}
+              />
+            </div>
 
             <button
               className="px-4 py-2 bg-green-500 text-white rounded-md text-sm font-medium"
