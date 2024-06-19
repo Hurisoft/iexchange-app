1837d23b1e9498cd1474ffaaf044daf2424aa901
Jovells <jovells.appiah@gmail.com>
2024-06-19 17:13:10 +0000

    modified button to render link with href prop

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -8,6 +8,7 @@ interface Props extends React.HTMLProps<HTMLButtonElement> {
   loading?: boolean;
   styles?: any;
   icon?: string;
+  href?: string; // Add href prop
 }
 
 const Button: FC<Props> = ({
@@ -18,6 +19,7 @@ const Button: FC<Props> = ({
   loading,
   styles,
   icon,
+  href, // Destructure href prop
   ...props
 }) => {
   const onClickHandler = (
@@ -29,13 +31,36 @@ const Button: FC<Props> = ({
     }
   };
 
+  if (href) {
+    return (
+      <a
+        href={href}
+        className={`bg-gradient-to-b from-[#3384D9] via-[#0051A6] to-[#073E66] text-white text-sm px-4 py-3 rounded-lg ${className}`}
+        style={styles}>
+        {loading ? (
+          "Loading..."
+        ) : (
+          <>
+            {text ? (
+              <div className="flex flex-row justify-center items-center space-x-3">
+                <span>{text}</span>
+                {icon && <Image src={icon} alt="earn" width={20} />}
+              </div>
+            ) : (
+              children
+            )}
+          </>
+        )}
+      </a>
+    );
+  }
+
   return (
     <button
       onClick={onClickHandler}
       className={`bg-gradient-to-b from-[#3384D9] via-[#0051A6] to-[#073E66] text-white text-sm px-4 py-3 rounded-lg ${className}`}
       disabled={loading || props.disabled} // Disable button when loading
-      style={styles}
-    >
+      style={styles}>
       {loading ? (
         "Loading..."
       ) : (
